12d37f85bd51322b399038ba3bca18dad147c02c
Adrien <contact@ebook-horizoncrypto.com>
2026-01-28 17:49:41 +0100

    fix(stripe): load dotenv for api keys and highlight subscription link

--- api/manage-subscription.js
+++ api/manage-subscription.js
@@ -1,5 +1,6 @@
 import Stripe from 'stripe';
 import { Resend } from 'resend';
+import 'dotenv/config';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const resend = new Resend(process.env.RESEND_API_KEY);
